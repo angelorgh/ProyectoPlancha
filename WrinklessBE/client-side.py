5d4a7ef8ca6d8e9ce2b794f15e2254835b032119
Angelo Gonzalez <renatoangelo4@gmail.com>
2022-12-30 20:08:56 -0400

    Cambio codigo comunicacion serial

--- WrinklessBE/client-side.py
+++ WrinklessBE/client-side.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
 import serial
-# ser = serial.Serial("/dev/ttyS0", 9600)
-ser = serial.Serial('COM5', 9600)
-message = ser.readline().decode("utf-8")
-print(message)
-ser.close()
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
