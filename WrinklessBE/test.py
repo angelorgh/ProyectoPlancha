8d312e193eda7cb2c7539fe88c855f2d4fb856da
Gabriel Santana <Gabriel Santana@LT-21198>
2023-01-05 19:32:29 -0400

    Cambiando test

--- WrinklessBE/test.py
+++ WrinklessBE/test.py
@@ -21,6 +21,7 @@ import asyncio
 import json
 from models.TempRules import TempRule
 from AI.Spect_ColorClassifier import SpectColorClassifier
+import serial
 async def write_to_file(filename):
     with open(filename, 'w+') as file:
         i = 0
@@ -45,19 +46,19 @@ async def main():
     read_task = asyncio.create_task(read_from_file(filename))
     # wait for both tasks to complete
     await asyncio.gather(write_task, read_task)
-def getTimeTemp(color):
-        # self.logging.debug('Event getTimeTemp fired')
-        f = open('./data/temprules.json')
-        rules = json.load(f)
-        try:
-            return TempRule(rules[color]).rulesstring
-        except Exception as e:
-            print(e)
-        finally:
-            print('fin')
 
+def writeToSerial(message):
+        print('Event writeToSerial fired')
+        try:
+            ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+            # ser.reset_input_buffer()
+            ser.write(message.encode('utf-8'))
+            # ser.reset_input_buffer()
+        except Exception as e:
+            print(f"Error enviando informacion a serial. Valor enviado{message}. InnerException: {e}")
+        finally:
+            print(f"Se ha enviado a serial correctamente. Valor: {message}")   
 if __name__ == '__main__':
     # run the main function
-    print(str(getTimeTemp('red')))
-    print(type(str(getTimeTemp('red'))))
-    # asyncio.run(main())
+    writeToSerial("200")
+    
