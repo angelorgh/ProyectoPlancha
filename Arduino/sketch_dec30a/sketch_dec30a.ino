12e6657dad3ef82f268e16cc49f312277c622a1a
Angelo González <renatoangelo4@gmail.com>
2023-01-05 00:20:12 -0400

    Cambios a HMI para async y arduino para sim

--- Arduino/sketch_dec30a/sketch_dec30a.ino
+++ Arduino/sketch_dec30a/sketch_dec30a.ino
@@ -1,30 +1,39 @@
 String nom = "Arduino";
-String msg;
+String msg = "";
 void setup() {
  	Serial.begin(9600);
 }
 void loop() {
  	readSerialPort();
  	if (msg == "Start") {
- 			sendData("332,163,283,602,1533,1955,89");
+    sendData("332,163,283,602,1533,1955,89");
+    simulate();
  	}
-	else
-	{
-		sendData(msg)
-	}
  	delay(500);
 }
 void readSerialPort() {
  	msg = "";
  	if (Serial.available()) {
- 			delay(10);
- 			while (Serial.available() > 0) {
- 					msg += (char)Serial.read();
- 			}
- 			Serial.flush();
+    delay(10);
+    while (Serial.available() > 0) {
+        msg += (char)Serial.read();
+    }
+    Serial.flush();
  	}
 }
-void sendData(msg) {
+void sendData(String msg) {
  	//write data
- 	Serial.print(msg);
+ 	Serial.print(msg + "\n");
+}
+
+void simulate(){
+  bool timer = false;
+  int num = 0;
+  while(timer = false){
+    sendData("temp " + random(50, 100));
+    num++;
+    if(num >= 14000){
+      timer = true;
+    }
+  }
 }
