ec841ae02ff80c1cb35e6774ef92a073a40bba90
Jjwot-Dud3 <1085855@est.intec.edu.do>
2023-02-04 20:14:17 -0400

    Modificacion para probar rasp y arduino comunicacion

--- Arduino/sketch_dec30a/sketch_dec30a.ino
+++ Arduino/sketch_dec30a/sketch_dec30a.ino
@@ -1,7 +1,7 @@
 int num = 0;
 void setup() {
+   Serial.begin(115200);
   while (!Serial);
- 	Serial.begin(9600);
   pinMode(8, OUTPUT);
 }
 void loop() {
@@ -12,25 +12,13 @@ void loop() {
   }
 }
 void readSerialPort() {
-  num = 0;
+  //num = 0;
   while(!Serial.available());
   num = Serial.parseInt();  
 }
 
 void sendData(int val1) {
- 	//write data
- 	Serial.print(val1);
+  //write data
+  Serial.print(val1);
   Serial.print('\n');
-}
-
-void simulate(){
-  bool timer = false;
-  int num = 0;
-  while(timer = false){
-    sendData("temp " + random(50, 100));
-    num++;
-    if(num >= 14000){
-      timer = true;
-    }
-  }
 }
--- WrinklessBE/test.py
+++ WrinklessBE/test.py
@@ -1,4 +1,3 @@
-
 import serial
 import time
 
@@ -10,14 +9,15 @@ def readFromSerial(serial):
 
 def writeToSerial(serial):
         print('Event writeToSerial fired')
-        serial.write(b'200x')
+        serial.write(b'200')
         print('Se ha enviado')
 if __name__ == '__main__':
     try:
-        ser = serial.Serial("/dev/ttyACM0", 9600, timeout=3000)  # Initialize serial connection
+        ser = serial.Serial("/dev/ttyACM0", 115200, timeout=3000)  # Initialize serial connection
     except:
-        ser = serial.Serial("/dev/ttyACM1", 9600, timeout=3000)
+        ser = serial.Serial("/dev/ttyACM1", 115200, timeout=3000)
     
     ser.reset_input_buffer()
     writeToSerial(ser)
+    time.sleep(0.1)
     print(readFromSerial(ser))
