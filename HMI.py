b1d7e12e424392f7939192577b541abef9a19234
Gabriel Santana <Gabriel Santana@LT-21198>
2023-03-29 21:29:37 -0400

    Comentando while

--- HMI.py
+++ HMI.py
@@ -55,11 +55,11 @@ def on_start_click():
     parsetemp = float("{:.2f}".format(float(result.split("%")[0])))
     print(f"Valor temperatura {parsetemp}- Valor arduino: {result}")
     value1.config(text=f"{parsetemp}°C")
-    while(progressbar.running):
-        result = asyncio.get_event_loop().run_until_complete(client.send_message("200"))
-        parsetemp = float("{:.2f}".format(float(result.split("%")[0])))
-        # print(f"Valor temperatura {parsetemp} - Valor arduino: {result}")
-        value1.config(text=f"{parsetemp}°C")
+    # while(progressbar.running):
+    #     result = asyncio.get_event_loop().run_until_complete(client.send_message("200"))
+    #     parsetemp = float("{:.2f}".format(float(result.split("%")[0])))
+    #     # print(f"Valor temperatura {parsetemp} - Valor arduino: {result}")
+    #     value1.config(text=f"{parsetemp}°C")
     #value1.config(text="")
 
 def cancel():
