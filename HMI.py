5a1717b0e5c0996a45b5e885bc258aab6a746fc3
Gabriel Santana <Gabriel Santana@LT-21198>
2023-04-04 19:52:09 -0400

    agregando prints

--- HMI.py
+++ HMI.py
@@ -64,6 +64,9 @@ def warmingup (timer = None, progreswd = None):
         print(f"Result warming up: {resultwarmingup}")
         id2 = root.after(1500, warmingup(timer, progreswd))
     if resultwarmingup == 'Planchando':
+        print(f"Entro a planchando valor de id:{id2}")
+        root.after_cancel(id2)
+        print(f"Entro a planchando valor de result:{resultwarmingup}, timer: {timer}, progress: {progreswd}")
         progreswd.destroy()
         timer = int(timer.split("%")[0])
         print(timer)
