f48538bbfc0f9154d66597ed26435a2581fddbf4
Gabriel Santana <Gabriel Santana@LT-21198>
2023-04-04 20:43:53 -0400

    Agregando print

--- HMI.py
+++ HMI.py
@@ -63,7 +63,9 @@ def warmingup ():
     resultwarmingup = asyncio.get_event_loop().run_until_complete(client.send_message("500"))
     if resultwarmingup == '':
         id2 = root.after(1500, warmingup)
-    if resultwarmingup == 'Planchando':
+    if resultwarmingup != '':
+        print(resultwarmingup)
+    if resultwarmingup.strip() == 'Planchando':
         print(f"Entro a planchando valor de result:{resultwarmingup}, timer: {timer}, progress: {progress_window}")
         progress_window.destroy()
         timer = int(timer.split("%")[0])
