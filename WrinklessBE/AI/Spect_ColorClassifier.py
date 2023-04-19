9282675fcec301cdddf548e2a1157e86a5d6cc5b
Gabriel Santana <Gabriel Santana@LT-21198>
2023-04-19 11:58:12 -0400

    Cambiando dataset

--- WrinklessBE/AI/Spect_ColorClassifier.py
+++ WrinklessBE/AI/Spect_ColorClassifier.py
@@ -2,13 +2,10 @@ import json
 from joblib import dump, load
 from os import path
 from sklearn.neural_network import MLPClassifier
-import os
-this_dir = os.path.dirname(__file__) # Path to loader.py
-# sys.path.append(os.path.join(this_dir, '../HMI.py'))
 
 class SpectColorClassifier:
 
-    colors:list = ["blue","green", "red"]
+    colors:list = ["blue","green","nocolor","red"]
 
     @staticmethod
     def load_data_from_json(file_path:str) -> dict:
@@ -21,7 +18,7 @@ class SpectColorClassifier:
             
         for obj in dataset:
             # features
-            input = map(lambda v: float(v), [obj["V"], obj["B"], obj["G"], obj["Y"], obj["O"], obj["R"], obj["temp"] ])
+            input = map(lambda v: int(float(v)), [obj["R"], obj["O"], obj["Y"], obj["G"], obj["B"], obj["V"], obj["temp"]])
             # etiqueta
             output = [idx for idx, label in enumerate(labels) if label == obj["label"]]
             # agregar al set de entrenamiento
@@ -34,52 +31,51 @@ class SpectColorClassifier:
     def classify(spect:list) -> tuple:
 
         model_input = list(map(lambda v: float(v), spect))
-        # model = load(f"{this_dir}/model.joblib")
-        if path.exists(f"{this_dir}/model.joblib"):
-            model = load(f"{this_dir}/model.joblib")
+        
+        if path.exists("./model.joblib"):
+            model = load("model.joblib")
         else:
             print("entrenando red neuronal...")
-            print(path.realpath(__file__))
             model = MLPClassifier(
-                hidden_layer_sizes=(3,12), 
+                hidden_layer_sizes=(100,30,),
                 activation="relu", 
                 solver="lbfgs", 
-                max_iter=5000
+                max_iter=5000,
             )
-            trainset = SpectColorClassifier.load_data_from_json(f"{this_dir}/train.json")
+            trainset = SpectColorClassifier.load_data_from_json("train.json")
             model.fit(trainset["inputs"], trainset["outputs"])
             dump(model, "model.joblib")
         
         prediction = model.predict([model_input])[0]
         return SpectColorClassifier.colors[prediction]
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     from time import sleep
-#     from random import randint
+    from time import sleep
+    from random import randint
 
-#     file_path = "test.json"
+    file_path = "test.json"
 
-#     realclass = []
-#     predictions = []
-    
-#     with open(file_path, "r") as json_file:
-#             testset = json.load(json_file)["dataset"]
-#             testlabels = sorted(set([obj["label"] for obj in testset]))
+    realclass = []
+    predictions = []
+
+    with open(file_path, "r") as json_file:
+            testset = json.load(json_file)["dataset"]
+            testlabels = sorted(set([obj["label"] for obj in testset]))
             
          
-#     for obj in testset:
+    for obj in testset:
         
-#             spect = list(map(lambda v: float(v), [obj["V"], obj["B"], obj["G"], obj["Y"], obj["O"], obj["R"], obj["temp"] ]))
-#             predicted = SpectColorClassifier.classify(spect)
+            spect = list(map(lambda v: int(float(v)), [obj["R"], obj["O"], obj["Y"], obj["G"], obj["B"], obj["V"], obj["temp"] ]))
+            predicted = SpectColorClassifier.classify(spect)
             
-#             real = [label for idx, label in enumerate(testlabels) if label == obj["label"]]
+            real = [label for idx, label in enumerate(testlabels) if label == obj["label"]]
          
-#             predictions.append(predicted)
-#             realclass.append(real)
-#             #print(f"{real}\t-> this is {predicted}")
+            predictions.append(predicted)
+            realclass.append(real)
+            print(f"{real}\t-> this is {predicted}")
     
-#     correctPredictions = sum([int(predictions[i] == ''.join(realclass[i])) for i in range(0,len(predictions))])
-#     acc = (correctPredictions/len(realclass))*100
-#     print(correctPredictions)
-#     print(acc)
+    correctPredictions = sum([int(predictions[i] == ''.join(realclass[i])) for i in range(0,len(predictions))])
+    acc = (correctPredictions/len(realclass))*100
+    print(correctPredictions)
+    print(acc)
